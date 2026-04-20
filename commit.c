37834bc1f557a64480c86064f14438ba4ed4aa3b
Varshini A <varshini.gt@email.com>
2026-04-20 14:29:35 +0000

    Phase 4: implement commit_create base

--- commit.c
+++ commit.c
@@ -194,8 +194,35 @@ int head_update(const ObjectID *new_commit) {
 //
 // Returns 0 on success, -1 on error.
 int commit_create(const char *message, ObjectID *commit_id_out) {
-    // TODO: Implement commit creation
-    // (See Lab Appendix for logical steps)
-    (void)message; (void)commit_id_out;
-    return -1;
+    Commit commit;
+    
+    if (tree_from_index(&commit.tree) != 0) return -1;
+    
+    if (head_read(&commit.parent) == 0) {
+        commit.has_parent = 1;
+    } else {
+        commit.has_parent = 0;
+    }
+    
+    strncpy(commit.author, pes_author(), sizeof(commit.author) - 1);
+    commit.author[sizeof(commit.author) - 1] = '\0';
+    
+    commit.timestamp = (uint64_t)time(NULL);
+    
+    strncpy(commit.message, message, sizeof(commit.message) - 1);
+    commit.message[sizeof(commit.message) - 1] = '\0';
+    
+    void *data;
+    size_t len;
+    if (commit_serialize(&commit, &data, &len) != 0) return -1;
+    
+    if (object_write(OBJ_COMMIT, data, len, commit_id_out) != 0) {
+        free(data);
+        return -1;
+    }
+    free(data);
+    
+    if (head_update(commit_id_out) != 0) return -1;
+    
+    return 0;
 }
